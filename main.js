6c74e9703eed7dd266838032a12173ec075c059d
delaquash <30915176+delaquash@users.noreply.github.com>
2018-12-29 01:11:54 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const left = document.querySelector(".left");
+const middle =document.querySelector(".middle");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
+
+
+left.addEventListener("mouseenter", () => {
+	container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", () => {
+	container.classList.remove("hover-left");
+});
+
+
+middle.addEventListener("mouseenter", () => {
+	container.classList.add("hover-middle")
+});
+
+middle.addEventListener("mouseleave", () => {
+	container.classList.remove("hover-middle")
+});
+
+
+
+right.addEventListener("mouseenter", () => {
+	container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+	container.classList.remove("hover-right");
+});
+
+
+
+
+
+// right.addEventListener('mouseenter', () => {
+//   container.classList.add("hover-right");
+// });
+// right.addEventListener('mouseenter', () => {
+// 	container.classList.add('hover-right');
+// });
+
+
+
+
+
